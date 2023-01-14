a03ac1a587a6b5f9d1c5e9a0f020ac3e20e647ad
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2023-01-13 21:19:49 -0300

    Update lexer.rs

--- lexer.rs
+++ lexer.rs
@@ -1,26 +1,26 @@
 use std::collections::HashMap;
 
-#[derive(PartialEq, Debug, Clone, Copy)]
-enum TokenType {
-    Symbol, Integer, Print, Null
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum TokenKind {
+    Symbol, Integer, Let, Eq
 }
 
 #[derive(Debug, PartialEq)]
-struct Token {
-    text: String,
-    typ: TokenType
+pub struct Token<'a> {
+    text: &'a str,
+    typ: TokenKind
 }
 
-impl Token {
-    fn new(text: String, typ: TokenType) -> Self {
+impl<'a> Token<'a> {
+    fn new(text: &'a str, typ: TokenKind) -> Self {
         Self {
-            text: text,
-            typ: typ
+            text,
+            typ
         }
     }
 }
 
-pub fn is_string_numeric(value: &String) -> bool {
+pub fn is_string_numeric(value: &str) -> bool {
     for character in value.chars() {
         if !character.is_numeric() {
             return false
@@ -29,41 +29,39 @@ pub fn is_string_numeric(value: &String) -> bool {
     return true
 }
 
-fn lex(code: String) -> Vec<Token> {
-    let keyws: HashMap<String, TokenType> = HashMap::from([(String::from("print"), TokenType::Print)]);
-    let mut tokens: Vec<Token> = vec![];
-    let mut buf = String::new();
+pub fn lex(code: &str) -> Vec<Token<'_>> {
+    let keyws: HashMap<&str, TokenKind> = HashMap::from([("let", TokenKind::Let)]);
+    let operators: HashMap<&str, TokenKind> = HashMap::from([("=", TokenKind::Eq)]);
+
+    let mut tokens: Vec<Token> = Vec::new();
+    //let mut buf = String::new();
+    let mut buf = 0;
     for c in code.chars() {
-        if !c.is_whitespace() {
+        if let Some(op) = operators.get(&*c.to_string()) {
+            tokens.push(Token::new(&*buf, TokenKind::Symbol));
+            tokens.push(Token::new(&*c.to_string(), *op));
+            buf.clear();
+        } else if !c.is_whitespace() {
             buf.push(c);
-        }
-        if c.is_whitespace() && !buf.is_empty() {
+        } else if c.is_whitespace() && !buf.is_empty() {
             if is_string_numeric(&buf) {
-                tokens.push(Token::new(buf.clone(), TokenType::Integer));
+                tokens.push(Token::new(&*buf, TokenKind::Integer));
+            } else if let Some(tt) = keyws.get(&*buf) {
+                tokens.push(Token::new(&*buf, *tt))
             } else {
-                if let Some(tt) = keyws.get(&buf) {
-                    tokens.push(Token::new(buf.clone(), *tt))
-                } else {
-                    tokens.push(Token::new(buf.clone(), TokenType::Symbol));
-                }
+                tokens.push(Token::new(&*buf, TokenKind::Symbol));
             }
             buf.clear();
         }
     }
     if !buf.is_empty() {
         if is_string_numeric(&buf) {
-            tokens.push(Token::new(buf.clone(), TokenType::Integer));
+            tokens.push(Token::new(&*buf, TokenKind::Integer));
+        } else if let Some(tt) = keyws.get(&*buf) {
+            tokens.push(Token::new(&*buf, *tt))
         } else {
-             if let Some(tt) = keyws.get(&buf) {
-                 tokens.push(Token::new(buf.clone(), *tt))
-             } else {
-                 tokens.push(Token::new(buf.clone(), TokenType::Symbol));
-             }
+            tokens.push(Token::new(&buf, TokenKind::Symbol));
         }
     }
-    return tokens;
-}
-
-fn main() {
-    println!("{:#?}", lex(String::from("print67")));
+    return tokens
 }
