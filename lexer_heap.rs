a8567d128f9be6fb2f3c2418fc79b46ba77b1533
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2023-01-22 18:58:46 -0300

    Update and rename lexer.rs to lexer_heap.rs

--- lexer_heap.rs
+++ lexer_heap.rs
@@ -6,17 +6,14 @@ enum TokenKind {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Token<'a> {
-    text: &'a str,
-    typ: TokenKind
+pub struct Token {
+    text: String,
+    kind: TokenKind
 }
 
-impl<'a> Token<'a> {
-    fn new(text: &'a str, typ: TokenKind) -> Self {
-        Self {
-            text,
-            typ
-        }
+impl Token {
+    fn new(text: String, kind: TokenKind) -> Self {
+        Self { text, kind }
     }
 }
 
@@ -29,38 +26,37 @@ pub fn is_string_numeric(value: &str) -> bool {
     return true
 }
 
-pub fn lex(code: &str) -> Vec<Token<'_>> {
+pub fn lex(code: String) -> Vec<Token> {
     let keyws: HashMap<&str, TokenKind> = HashMap::from([("let", TokenKind::Let)]);
     let operators: HashMap<&str, TokenKind> = HashMap::from([("=", TokenKind::Eq)]);
 
     let mut tokens: Vec<Token> = Vec::new();
-    //let mut buf = String::new();
-    let mut buf = 0;
+    let mut buf = String::new();
     for c in code.chars() {
         if let Some(op) = operators.get(&*c.to_string()) {
-            tokens.push(Token::new(&*buf, TokenKind::Symbol));
-            tokens.push(Token::new(&*c.to_string(), *op));
+            tokens.push(Token::new(buf.clone(), TokenKind::Symbol));
+            tokens.push(Token::new(c.to_string(), *op));
             buf.clear();
         } else if !c.is_whitespace() {
             buf.push(c);
         } else if c.is_whitespace() && !buf.is_empty() {
             if is_string_numeric(&buf) {
-                tokens.push(Token::new(&*buf, TokenKind::Integer));
+                tokens.push(Token::new(buf.clone(), TokenKind::Integer));
             } else if let Some(tt) = keyws.get(&*buf) {
-                tokens.push(Token::new(&*buf, *tt))
+                tokens.push(Token::new(buf.clone(), *tt))
             } else {
-                tokens.push(Token::new(&*buf, TokenKind::Symbol));
+                tokens.push(Token::new(buf.clone(), TokenKind::Symbol));
             }
             buf.clear();
         }
     }
     if !buf.is_empty() {
         if is_string_numeric(&buf) {
-            tokens.push(Token::new(&*buf, TokenKind::Integer));
+            tokens.push(Token::new(buf.clone(), TokenKind::Integer));
         } else if let Some(tt) = keyws.get(&*buf) {
-            tokens.push(Token::new(&*buf, *tt))
+            tokens.push(Token::new(buf.clone(), *tt))
         } else {
-            tokens.push(Token::new(&buf, TokenKind::Symbol));
+            tokens.push(Token::new(buf.clone(), TokenKind::Symbol));
         }
     }
     return tokens
